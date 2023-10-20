5680737ac0982b06b93eecc70eb4cbe721b43ec6
bavuchoko@naver.com <bavuchoko@naver.com>
2023-10-20 14:44:21 +0900

    create content

--- src/components/utils/SelectBox.js
+++ src/components/utils/SelectBox.js
@@ -1,9 +1,32 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Select from "react-select";
-function SelectBox({categories, setSelectValue}) {
+import {getCategoryList} from "../../api/category/CateogryService";
+function SelectBox({options, setSelectValue}) {
 
     const selectInputRef = useRef(null);
+    const list = [];
 
+
+    const mapOptionsRecursively = (options) => {
+        if(options.length > 0){
+            options.map(option => {
+            const mappedOption = {
+                value: option.name,
+                label: option.name
+            };
+            list.push(mappedOption)
+                if (option.child) {
+                    mapOptionsRecursively(option.child);
+                }
+            });
+        }
+        return list
+    };
+
+    useEffect(() => {
+        mapOptionsRecursively(options);
+    }, [options]);
+    
     return (
         <>
             <Select
@@ -15,7 +38,7 @@ function SelectBox({categories, setSelectValue}) {
                         setSelectValue("");
                     }
                 }}
-                options={categories}
+                options={list}
                 placeholder="카테고리"
             />
         </>
--- src/packages/content/content/Create.js
+++ src/packages/content/content/Create.js
@@ -1,54 +1,88 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import Editor from "ckeditor5-custom-build/build/ckeditor";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import SelectBox from "../../../components/utils/SelectBox";
 import {useNavigate} from "react-router-dom";
+import {getCategoryList} from "../../../api/category/CateogryService";
+import axios from "axios";
+import {noAuh} from "../../../api/instance/Instance";
 
 
-const editorConfiguration = {
-    placeholder:"내용을 입력하세요",
-    toolbar: {
-        items: [
-            "undo",
-            "redo",
-            "|",
-            "heading",
-            "|",
-            "fontfamily",
-            "fontsize",
-            "|",
-            "alignment",
-            "|",
-            "bold",
-            "italic",
-            "strikethrough",
-            "underline",
-            "|",
-            "link",
-            "|",
-            "outdent",
-            "indent",
-            "|",
-            "code",
-            "codeBlock",
-            "|",
-            "insertTable",
-            "|",
-            "imageUpload",
-            "blockQuote",
-        ],
-        shouldNotGroupWhenFull: true
-    }
-};
 function Create(props) {
+
+
+    const editorConfiguration = {
+        extraPlugins: [uploadPlugin],
+        placeholder:"내용을 입력하세요",
+        toolbar: {
+            items: [
+                "undo",
+                "redo",
+                "|",
+                "heading",
+                "|",
+                "fontfamily",
+                "fontsize",
+                "|",
+                "alignment",
+                "|",
+                "bold",
+                "italic",
+                "strikethrough",
+                "underline",
+                "|",
+                "link",
+                "|",
+                "outdent",
+                "indent",
+                "|",
+                "code",
+                "codeBlock",
+                "|",
+                "insertTable",
+                "|",
+                "imageUpload",
+                "blockQuote",
+            ],
+            shouldNotGroupWhenFull: true
+        }
+    };
     const navigate = useNavigate();
     const [tag, setTag] = useState('');
     const [tags, setTags] = useState([]);
-
+    const [category, setCategory] = useState([]);
+    const [title, setTitle] = useState('');
+    const [htmlBody, setHtmlbody] = useState('');
+    const categories = JSON.parse(localStorage.getItem('category'))
     const tagHandler = (e) => {
         setTag(e.target.value);
     };
+    const customUploadAdapter = (loader) => {
+        return {
+            upload() {
+                return new Promise((resolve, reject) => {
+                    const formData = new FormData();
+                    loader.file.then((file) => {
+                        formData.append("file", file);
 
+                        noAuh.post("http://localhost:8080/api/v0/file/upload", formData)
+                            .then((res) => {
+                                resolve({
+                                    default: res.data.data.uri,
+                                });
+                            })
+                            .catch((err) => reject(err));
+                    });
+                });
+            },
+        };
+    };
+
+    function uploadPlugin(editor) {
+        editor.plugins.get("FileRepository").createUploadAdapter = (loader) => {
+            return customUploadAdapter(loader);
+        };
+    }
     const tagEnter = (e) => {
         if (e.key === 'Enter') {
             if (tag.trim() !== '') {
@@ -67,22 +101,49 @@ function Create(props) {
         setTags(updatedTags);
     };
 
+    const titleChange =(e) =>{
+        setTitle(e.target.value)
+    }
+
     const goList =() =>{
         navigate('/content')
     }
+
+
+    const onSubmit = () => {
+        let body =''
+        body = htmlBody.replace(/<br\/>/ig, "\n");
+        body = body.replace(/<(\/)?([a-zA-Z]*)(\s[a-zA-Z]*=[^>]*)?(\s)*(\/)?>/ig, "");
+        body = body.replace(/(<([^>]+)>)/gi, "");
+        body = body.replace(/&nbsp;/gi,"");
+
+        const content = {
+            title: title,
+            body: body,
+            bodyHtml: htmlBody,
+            category:category,
+            tags:tags
+        }
+        console.log(content)
+    }
+
+
     return (
         <>
             <div className="create-header fixed"></div>
             <div className="content-containers">
                 <div className="content-create-gategory">
-                <SelectBox />
+                <SelectBox options={categories} setSelectValue={setCategory}/>
                 </div>
-                <input type={"text"} className="content-title" placeholder={'제목을 입력하세요'}/>
+                <input type={"text"}
+                       onChange={titleChange}
+                       value={title}
+                       className="content-title" placeholder={'제목을 입력하세요'}/>
                 <CKEditor
                     editor={Editor}
                     config={editorConfiguration}
                     onChange={(event, editor) => {
-                        const data = editor.getData();
+                        setHtmlbody(editor.getData());
                     }}
                 />
 
@@ -115,7 +176,7 @@ function Create(props) {
 
                 <div className="wrap_btn">
                     <button id="publish-layer-btn" className="btn-content-cancel" onClick={goList} >취소</button>
-                    <button id="publish-layer-btn" className="btn-content-submit">완료</button>
+                    <button id="publish-layer-btn" className="btn-content-submit" onClick={onSubmit}>완료</button>
                 </div>
             </div>
         </>
